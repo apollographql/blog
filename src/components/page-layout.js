520dd5e4984b8208c2e495943abd135c05e85d74
Trevor Blades <tdblades@gmail.com>
2020-01-28 16:59:21 -0800

    Add search box

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -2,23 +2,31 @@ import NewsletterForm from './newsletter-form';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
+import {IconSearch} from '@apollo/space-kit/icons/IconSearch';
 import {Layout} from 'gatsby-theme-apollo-core';
+import {TextField} from '@apollo/space-kit/TextField';
 
 const Wrapper = styled.div({
   display: 'flex',
   flexDirection: 'column',
   maxWidth: 1220,
   minHeight: '100vh',
-  margin: '0 auto'
+  margin: '0 auto',
+  padding: '0 40px'
 });
 
 const Header = styled.header({
   display: 'flex',
   alignItems: 'center',
-  height: 64,
+  padding: '16px 0',
   backgroundColor: 'white',
   position: 'sticky',
   top: 0
+});
+
+const SearchInput = styled(TextField)({
+  flexGrow: 1,
+  marginLeft: 64
 });
 
 const InnerWrapper = styled.div({
@@ -41,7 +49,18 @@ export default function PageLayout(props) {
       <Wrapper>
         <Header>
           Logo
-          <div>Search</div>
+          <SearchInput
+            size="large"
+            placeholder="Search blog..."
+            icon={
+              <IconSearch
+                style={{
+                  height: 14,
+                  width: 14
+                }}
+              />
+            }
+          />
         </Header>
         <InnerWrapper>
           <Main>{props.children}</Main>
