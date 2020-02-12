d68c0d744ec23a38555ca25dd1e704dd39ed80d8
Trevor Blades <tdblades@gmail.com>
2020-02-11 17:09:29 -0800

    Wire up marketo script/form

--- src/components/newsletter-form.js
+++ src/components/newsletter-form.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import {Button} from '@apollo/space-kit/Button';
+import React, {useEffect} from 'react';
 import {SectionHeading, SidebarSection} from './ui';
-import {TextField} from '@apollo/space-kit/TextField';
-import {colors} from '@apollo/space-kit/colors';
 
-const StyledInput = styled(TextField)({
-  marginBottom: 16
-});
+// import styled from '@emotion/styled';
+// import {Button} from '@apollo/space-kit/Button';
+// import {TextField} from '@apollo/space-kit/TextField';
+// import {colors} from '@apollo/space-kit/colors';
 
-const StyledButton = styled(Button)({
-  width: '100%'
-});
+// const StyledInput = styled(TextField)({
+//   marginBottom: 16
+// });
+
+// const StyledButton = styled(Button)({
+//   width: '100%'
+// });
 
 export default function NewsletterForm() {
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  useEffect(() => {
+    if (window.MktoForms2) {
+      window.MktoForms2.loadForm(
+        '//app-ab16.marketo.com',
+        '627-RVJ-941',
+        1341,
+        form => {
+          form.onSuccess(() => {
+            form.getFormElem().hide();
+            return false;
+          });
+        }
+      );
+    }
+  }, []);
 
   return (
     <SidebarSection>
-      <form onSubmit={handleSubmit}>
-        <SectionHeading>Mailing list</SectionHeading>
-        <h5 style={{marginBottom: 24}}>
-          Don’t miss a single post! Be the first to hear about meetups and other
-          news.
-        </h5>
-        <StyledInput size="large" placeholder="Your email address" />
+      <SectionHeading>Mailing list</SectionHeading>
+      <h5 style={{marginBottom: 24}}>
+        Don’t miss a single post! Be the first to hear about meetups and other
+        news.
+      </h5>
+      <form id="mktoForm_1341" />
+      {/* <StyledInput size="large" placeholder="Your email address" />
         <StyledButton
           type="submit"
           color={colors.indigo.dark}
           style={{height: 40}}
         >
           Subscribe
-        </StyledButton>
-      </form>
+        </StyledButton> */}
     </SidebarSection>
   );
 }
