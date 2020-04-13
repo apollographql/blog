0b6a4c29d7f213b260a5296a599667a8deec2643
Trevor Blades <tdblades@gmail.com>
2020-04-13 11:29:09 -0700

    Set category order in WP

--- src/components/categories.js
+++ src/components/categories.js
@@ -2,20 +2,14 @@ import React from 'react';
 import {Category, CategoryNav, SectionHeading, SidebarSection} from './ui';
 import {graphql, useStaticQuery} from 'gatsby';
 
-const categories = [
-  'announcement',
-  'frontend',
-  'backend',
-  'platform',
-  'how-to',
-  'community'
-];
-
 export default function Categories() {
   const data = useStaticQuery(
     graphql`
       {
-        allWordpressCategory(filter: {slug: {ne: "uncategorized"}}) {
+        allWordpressCategory(
+          filter: {slug: {ne: "uncategorized"}}
+          sort: {order: ASC, fields: acf___menu_order}
+        ) {
           nodes {
             id
             slug
@@ -29,14 +23,9 @@ export default function Categories() {
     <SidebarSection>
       <SectionHeading>Categories</SectionHeading>
       <CategoryNav>
-        {data.allWordpressCategory.nodes
-          .slice()
-          .sort(
-            (a, b) => categories.indexOf(a.slug) - categories.indexOf(b.slug)
-          )
-          .map(category => (
-            <Category key={category.id} category={category} />
-          ))}
+        {data.allWordpressCategory.nodes.map(category => (
+          <Category key={category.id} category={category} />
+        ))}
       </CategoryNav>
     </SidebarSection>
   );
