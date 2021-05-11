2e0d56ab150d5355cc8b2b8a2a78795477b6a5f8
Trevor Blades <tdblades@gmail.com>
2021-05-11 12:13:02 -0700

    Add syntax highlighting for python and sql

--- src/components/post-template/post-content.js
+++ src/components/post-template/post-content.js
@@ -22,6 +22,8 @@ import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-graphql';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-sql';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-typescript';
 
