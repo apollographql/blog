e4845eeeff5c971fea8baff3560f190b9701a81e
Jay Chesterman <jay.chesterman@live.ca>
2021-05-26 13:27:23 -0400

    Adding utm grabber script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 export const onRenderBody = ({setHeadComponents}) => {
-  setHeadComponents([
-    <script
-      key="feedback"
-      dangerouslySetInnerHTML={{
-        __html: `
+  setHeadComponents(
+    [
+      <script
+        key="feedback"
+        dangerouslySetInnerHTML={{
+          __html: `
           var ffWidgetId = '4220d7fc-6634-43f6-8261-fe5d270ede6a';
           var ffWidgetScript = document.createElement("script");
           ffWidgetScript.type = "text/javascript";
           ffWidgetScript.src = 'https://freddyfeedback.com/widget/freddyfeedback.js';
           document.head.appendChild(ffWidgetScript);
         `
-      }}
-    />
-  ]);
+        }}
+      />
+    ],
+    <script src="https://apollographql.com/utm-grabber.js" />
+  );
 };
