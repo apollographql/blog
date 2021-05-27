8af93efc886433ab109307a78aac1b456feaec52
Trevor Blades <tdblades@gmail.com>
2021-05-26 17:04:55 -0700

    Put utm script inside array

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
 export const onRenderBody = ({setHeadComponents}) => {
-  setHeadComponents(
-    [
-      <script
-        key="feedback"
-        dangerouslySetInnerHTML={{
-          __html: `
+  setHeadComponents([
+    <script
+      key="feedback"
+      dangerouslySetInnerHTML={{
+        __html: `
           var ffWidgetId = '4220d7fc-6634-43f6-8261-fe5d270ede6a';
           var ffWidgetScript = document.createElement("script");
           ffWidgetScript.type = "text/javascript";
           ffWidgetScript.src = 'https://freddyfeedback.com/widget/freddyfeedback.js';
           document.head.appendChild(ffWidgetScript);
         `
-        }}
-      />
-    ],
+      }}
+    />,
     <script key="utm" src="https://www.apollographql.com/utm-grabber.js" />
-  );
+  ]);
 };
