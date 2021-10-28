658861469c0993e6039ebc78bc79df6f5d7deb3b
Trevor Blades <tdblades@gmail.com>
2021-10-28 09:24:43 -0700

    Add rust syntax highlighting

--- src/components/post-template/post-content.js
+++ src/components/post-template/post-content.js
@@ -26,6 +26,7 @@ import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-sql';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-rust';
 
 const DOUBLE_WRAPPER_PADDING_X = WRAPPER_PADDING_X * 2;
 const ALIGNFULL_WIDTH = 'var(--rw, 100vw)';
